7ef4bec2dc525825acea991194d94a17849061ce
LucasIsidoroGalusni <isidorogalusni@gmail.com>
2021-06-23 00:36:15 +0000

    acabei essa parte
    Signed-off-by: LucasIsidoroGalusni <isidorogalusni@gmail.com>

--- pwirea2/src/App.js
+++ pwirea2/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import IndiretaPai from './components/basico/IndiretaPai'
+import Estado from './components/basico/Estado' 
+import FamiliaClone from './components/basico/FamiliaClone'
+import FamiliaMembroClone from './components/basico/MembroFamiliaClone'
+import Familia from './components/basico/Familia'
+import Card from './components/layout/Card'
+import Propriedades from './components/basico/Propriedades'
+import OlaMundo from './components/basico/OlaMundo'
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App" >
+        <h1>Exemplos React</h1>
+
+        <div className="Cards" >
+
+          <Card titulo="Comunicação Indireta (de filho para pai)" cor="#f7673b">
+            <IndiretaPai />
+          </Card>
+
+
+          <Card titulo="Estado" cor="#ffb400">
+            <Estado />
+          </Card>
+
+          <Card titulo="Componentes aninhados (CloneElement)" cor="#82212e">
+            <FamiliaClone sobrenome="Simpson">
+              < FamiliaMembroClone nome="Homer Jay" />
+              < FamiliaMembroClone nome="Marjorie" />
+              < FamiliaMembroClone nome="Batholomew JoJo" />
+              < FamiliaMembroClone nome="Lisa Marie" />
+              < FamiliaMembroClone nome="Margareth" />
+            </FamiliaClone>
+          </Card>
+
+          <Card titulo="Componentes aninhados" cor="#d1495b">
+            <Familia sobrenome="Simpson"/>
+          </Card>
+
+          <Card titulo="Propriedades" cor="#91cb3e">
+            <Propriedades nome="Homer" sobrenome="Jay Simpson" idade={38}/>
+          </Card>
+
+          <Card titulo="Ola Mundo!" cor="#226ce0">
+            <OlaMundo />
+          </Card>
+            
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
